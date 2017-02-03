0e4b638f04993c751bd8cefaf6609bef918088f3
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2017-02-03 18:47:01 +0100

    rustc_llvm: Default to libc++ on FreeBSD
    ... instead of libstdc++. FreeBSD is using LLVM's libc++ by default now.

--- src/librustc_llvm/build.rs
+++ src/librustc_llvm/build.rs
@@ -234,6 +234,8 @@ fn main() {
     // OpenBSD has a particular C++ runtime library name
     let stdcppname = if target.contains("openbsd") {
         "estdc++"
+    } else if target.contains("freebsd") {
+        "c++"
     } else {
         "stdc++"
     };
